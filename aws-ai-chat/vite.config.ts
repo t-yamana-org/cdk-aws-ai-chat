bece899c4ba5f04d75b19fa8a4c747f86f6f048d
t-yamana-mba <gannbarimonogatari@gmail.com>
2025-12-28 07:19:44 +0000

    chore: biome works

--- aws-ai-chat/vite.config.ts
+++ aws-ai-chat/vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',  // ホストマシンからアクセス可能にする
-    // NOTE: 通常の localhost だと不可
-    port: 5173
-  }
-})
+	plugins: [react()],
+	server: {
+		host: "0.0.0.0", // ホストマシンからアクセス可能にする
+		// NOTE: 通常の localhost だと不可
+		port: 5173,
+	},
+});
