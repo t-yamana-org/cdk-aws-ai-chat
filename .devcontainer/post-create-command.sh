d5b0d374b5982a97e8f902b05fa48faf2d34b243
t-yamana-mba <gannbarimonogatari@gmail.com>
2025-12-30 10:31:58 +0000

    env: git-secrets tested and installed

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -3,6 +3,15 @@
 # CDK インストール
 npm install -g aws-cdk
 
+# git-secrets インストール
+git clone https://github.com/awslabs/git-secrets.git /tmp/git-secrets
+cd /tmp/git-secrets && sudo make install
+
+# このリポジトリに git-secrets を設定
+cd /workspaces/cdk-aws-ai-chat
+git secrets --install
+git secrets --register-aws
+
 # FIXME: kiro-cli のインストール方法は未確定
 
 # スクリプトが成功したことを示すために、終了コード0で終了
