cc4dc551993ad19806409e54d9a1fb257fe1b20c
t-yamana-mba <gannbarimonogatari@gmail.com>
2026-03-15 16:39:01 +0000

    fix: custom identifier removed

--- frontend/amplify/data/resource.ts
+++ frontend/amplify/data/resource.ts
@@ -4,23 +4,17 @@ const schema = a.schema({
 	Message: a
 		.model({
 			conversationId: a.id().required(),
-			createdAt: a.datetime().required(),
 			conversation: a.belongsTo("Conversation", "conversationId"),
 			sender: a.string(),
 			content: a.string(),
 		})
-		.identifier(["conversationId", "createdAt"])
 		.authorization((allow) => [allow.owner()]),
 
 	Conversation: a
 		.model({
-			conversationId: a.id().required(),
 			title: a.string().required(),
-			createdAt: a.datetime(),
-			updatedAt: a.datetime(),
 			messages: a.hasMany("Message", "conversationId"),
 		})
-		.identifier(["conversationId"])
 		.authorization((allow) => [allow.owner()]),
 });
 
